82873128afdc88a744bdc29ffd7e45395cef988e
KayleeMcLaren <69905377+KayleeMcLaren@users.noreply.github.com>
2021-08-15 11:40:08 +0200

    Create life_in_weeks.py
    Calculates how many days, weeks, and months a user has left to live, assuming they'll live to 90 years old.

---
+++ Life in Weeks/life_in_weeks.py
@@ -0,0 +1,9 @@
+int(input("What is your current age? "))
+
+years_remaining = 90 - age
+days = years_remaining * 365
+weeks = years_remaining * 52
+months = years_remaining * 12
+
+
+print(f"You have {days} days, {weeks} weeks, and {months} months to live")
